27f96954a48aec6c25adf3a3d75538332a4fca96
bhshp <shp_123@foxmail.com>
2021-11-17 17:27:56 +0800

    fix setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='muwgs',
-    version='1.0.0 beta',
+    version='1.0.0',
+    keywords=['Microsoft Ultimate Word Games', 'script', 'python'],
+    description='muwg script',
+    long_description='Python Script for automatic playing Microsoft Ultimate Word Games',
+    license='MIT License',
+
+    url='https://github.com/bhshp/muwgs.git',
+    author='bhshp',
+    author_email='shp_123@foxmail.com',
+
+    include_package_data=True,
+    platforms='Windows 10 or above',
     packages=find_packages(),
-    license='MIT LICENSE'
+    requires=[]
 )
